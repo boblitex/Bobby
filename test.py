bd3ea76fdcad5da7a21c26d54a5bb9b28c6c31ac
Jude Bobinihi <boblitejude@gmail.com>
2018-09-01 00:19:53 +0200

    create analogy

---
+++ test.py
@@ -0,0 +1,13 @@
+def kdkd(x, y):
+    if (x == y):
+        print(x + " is equal to : " + y)
+    elif (x > y):
+        print(x + " is greater than " + y)
+    else:
+        print(y + " is greater than " + x)
+
+
+s = input("enter number 1 ")
+m = input("enter number 2 ")
+
+kdkd(s, m)
